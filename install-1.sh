6629289f71c6f92f2765374233c21dd8c319105e
Rodolphe Stoclin <srodolphe@gmail.com>
2019-11-10 18:08:45 +0100

    Add Grub Theme

--- install-1.sh
+++ install-1.sh
@@ -307,6 +307,12 @@ sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=\/dev\/nvme0n1p6
 #mkdir -p /mnt/windows
 #mount /dev/nvme0n1p4 /mnt/windows
 
+cp -R themes/razer /boot/grub/themes
+
+echo 'GRUB_GFXMODE="1920x1080-32"' >> /etc/default/grub
+echo 'GRUB_GFXPAYLOAD_LINUX="1920x1080-32"' >> /etc/default/grub
+echo 'GRUB_THEME="/boot/grub/themes/razer/theme.txt"' >> /etc/default/grub
+
 grub-mkconfig -o /boot/grub/grub.cfg
 
 
